d25cb31c10442da72a680c690dea87a0adb735a8
Evan <evanbennett83@gamil.com>
2024-01-03 09:45:59 -0500

    Added limited seed data for testing routes

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,28 +1,45 @@
-const { Budgets, User } = require('../models');
+const { Budgets, User, Transactions } = require('../models');
 const sequelize = require('../config/connection');
+
+const userData = [
+  {
+    first_name: "John",
+    last_name: "Doe",
+    email: "john.doe@email.com",
+    password: "12345678"
+  },
+  // Add more users as needed
+];
+
+const seedUser = () => User.bulkCreate(userData);
 
 const budgetData = [
   {
     name: 'Budget 1',
     amount: 500,
     user_id: 1,
-  }
-  // Add more budgets as needed
-];
-const userData = [
-  {
-    first_name: "new",
-    last_name: "name",
-    email: "emaasdfil@email.com",
-    password: "12345asdf78"
-  }
+  },
   // Add more budgets as needed
 ];
 
-const seedUser = () => User.bulkCreate(userData);
 const seedBudgets = () => Budgets.bulkCreate(budgetData);
+
+const transactionData = [
+  {
+    is_expense: true,
+    transaction_amount: 100,
+    user_id: 1,
+    budget_id: 1,
+  },
+  // Add more transactions as needed
+];
+
+const seedTransactions = () => Transactions.bulkCreate(transactionData);
 
 sequelize.sync({ force: true }).then(() => {
   seedUser()
-  seedBudgets()
-})
+    .then(() => seedBudgets())
+    .then(() => seedTransactions())
+    .then(() => console.log('Database seeded!'))
+    .catch((error) => console.log(error));
+});
