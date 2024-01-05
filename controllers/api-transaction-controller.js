0cdfaf8e57d14417022d20872e827a1eea292446
Ronald Martin <ronaldmartin02@gmail.com>
2024-01-04 23:43:19 -0500

    added api functions

--- controllers/api-transaction-controller.js
+++ controllers/api-transaction-controller.js
@@ -13,7 +13,44 @@ const getAllTransactions = async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+const getTransaction = async (req, res) => {
+  try {
+    const transactionId = req.params.id;
+    const getOneTransaction = await Transactions.findByPk(transactionId);
+    if (!getOneTransaction) {
+      return res.status(404).json('No Transaction found with that ID');
+    }
+    res.json(getOneTransaction);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+const postTransaction = async (req, res) => {
+  try{
+    const { isExpense, transactionAmount, userId, budgetId} = req.body;
 
+    const newTransaction = await Transactions.create({
+      is_expense: isExpense,
+      transaction_amount: transactionAmount,
+      user_id: userId,
+      budget_id: budgetId,
+    });
+  } catch(error){
+
+  }
+}
+const deleteTransaction = async (req, res) => {
+  try {
+    const deleteTransaction = await Transactions.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json('Transaction deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 module.exports = {
   getAllTransactions,
 };
