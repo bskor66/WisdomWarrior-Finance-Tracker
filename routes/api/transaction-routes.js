445da086b32a0567a94cf43cce0956b03642882a
Ronald Martin <ronaldmartin02@gmail.com>
2024-01-07 23:54:10 -0500

    Added update route

--- routes/api/transaction-routes.js
+++ routes/api/transaction-routes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const transactionController = require('../../controllers/api-transaction-controller');
 
 router.get('/', transactionController.getAllTransactions);
+router.get('/:id', transactionController.getTransaction);
+router.get('/', transactionController.createTransaction);
+router.get('/:id', transactionController.updateTransaction);
+router.get('/:id', transactionController.deleteTransaction);
 
 module.exports = router;
