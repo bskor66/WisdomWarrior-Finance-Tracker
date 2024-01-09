18bd26b343854bbd5cef01c64bcb1c9cd352580b
Evan <evanbennett83@gmail.com>
2024-01-09 18:23:38 -0500

    added delete transaction function.

--- public/js/budgets/budget-id.js
+++ public/js/budgets/budget-id.js
@@ -3,19 +3,34 @@ const dollarAmountRemaining = document.querySelector('#dollarAmountRemaining');
 const transactionExpense = document.querySelectorAll('[data-expense]');
 const transactionIncome = document.querySelectorAll('[data-income]');
 const createdAt = document.querySelectorAll('.created-at');
+const deleteTransaction = document.querySelectorAll('.delete-transaction');
 // console.log(transactionExpense)
-console.log(createdAt)
+
+deleteTransaction.forEach((transaction) => {
+  transaction.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const id = transaction.dataset.id;
+    console.log(id)
+    const response = await fetch(`/api/transactions/${id}`, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      alert('Failed to delete transaction');
+    }
+  });
+});
 
 createdAt.forEach((date) => {
   const dateCreated = date.dataset.timestamp;
-  console.log(dateCreated);
   const originalDate = new Date(dateCreated);
 
   const year = originalDate.getFullYear().toString().slice(2); // Get the last two digits of the year
   const month = (originalDate.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
   const day = originalDate.getDate().toString().padStart(2, '0');
   let hours = originalDate.getHours();
-  
+
   const ampm = hours >= 12 ? 'PM' : 'AM';
 
   hours = hours % 12 || 12;
@@ -33,7 +48,6 @@ transactionExpense.forEach((transaction) => {
   const expense = parseInt(transaction.dataset.expense);
   expenseTotal += expense;
 });
-console.log(expenseTotal);
 
 let incomeTotal = 0
 transactionIncome.forEach((transaction) => {
@@ -41,10 +55,9 @@ transactionIncome.forEach((transaction) => {
   incomeTotal += income;
 });
 
-let remaining = incomeTotal - expenseTotal;
-console.log(remaining);
+let remaining = dollarAmountRemaining.dataset.remaining - expenseTotal;
 
-dollarAmountRemaining.textContent = remaining;
+dollarAmountRemaining.textContent = remaining || dollarAmountRemaining.dataset.remaining;
 
 
 backToBudgets.addEventListener('click', () => {
